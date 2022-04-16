6403d0d82197e3dba915635991b28e9f62a86ebd
34-anish <luckyanish47@gmail.com>
2022-04-16 09:51:34 +0545

    Messaging

--- client/src/Components/message/Message.jsx
+++ client/src/Components/message/Message.jsx
@@ -1,12 +1,13 @@
 import "./message.css";
-function Message({ own }) {
+import { format } from "timeago.js";
+function Message({ message, own }) {
   return (
     <div className={own ? "message own" : "message"}>
       <div className="messageTop">
-        <img className="messageImg" src="/assets/no_pp.jpg" alt="" />
-        <p className="messageText">lorem50 </p>
+        {/* <img className="messageImg" src="/assets/no_pp.jpg" alt="" /> */}
+        <p className="messageText">{message.text} </p>
       </div>
-      <div className="messageBottom"> 1 hour ago</div>
+      <div className="messageBottom">{format(message.createdAt)}</div>
     </div>
   );
 }
